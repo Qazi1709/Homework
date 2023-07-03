c337fc57df71381d203a03fe590cef69855888ba
Qazi1709 <hassansiddiqui1709@gmail.com>
2023-07-03 10:27:49 +0530

    Matrics

---
+++ Matrices/1.js
@@ -0,0 +1,19 @@
+//Multi Dimensional Arrays
+
+
+/*
+
+1D - Arrays
+2D - Matrices
+3D and So on... - Tensors 
+
+*/
+
+
+let arr = [1, 2, 3, 4, 5, 6] //1-D Array
+
+let arr2 = [[1, 2], [3, 4], [5, 6]]; //2-D Array
+console.log(arr2[0][1]);
+
+let arr3 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+console.log(arr3[0][0][0]);
